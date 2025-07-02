166b26c1c9c122471bfba439354a9d2b412580ab
Phuong Nguyen <phuongah@likelion.net>
2025-07-02 10:14:50 +0700

    Add the template for greeting

--- src/main/java/com/likelion/ktc/lab23mvc/controller/GreetingController.java
+++ src/main/java/com/likelion/ktc/lab23mvc/controller/GreetingController.java
@@ -2,11 +2,12 @@ package com.likelion.ktc.lab23mvc.controller;
 
 import com.likelion.ktc.lab23mvc.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreetingController {
     private final GreetingService greetingService;
 
@@ -16,7 +17,9 @@ public class GreetingController {
     }
 
     @GetMapping("/greet")
-    public String greet(@RequestParam(defaultValue = "World") String name) {
-        return greetingService.getGreeting(name);
+    public String greet(@RequestParam(defaultValue = "World") String name, Model model) {
+        String greeting = greetingService.getGreeting(name);
+        model.addAttribute("greeting", greeting);
+        return "greeting";
     }
 } 
